32db01e4ead6c506473bf68c4b0405e18928decb
EloiseChakour <eloise.chakour@gmail.com>
2018-03-29 15:27:39 -0400

    Added Spearman & Comments

--- Correlationnal_Formulae.py
+++ Correlationnal_Formulae.py
@@ -11,7 +11,7 @@ Created on Mon Mar  5 16:41:52 2018
 import numpy as np
 #from openpyxl import load_workbook
 
-
+#Takes as input the excel sheet and outputs an array containing the diversity indices of each class.  
 def smallReshapeData(indexSheet, size):
     
     Array = []
@@ -22,21 +22,29 @@ def smallReshapeData(indexSheet, size):
     
     return Array
 
+#Test Arrays
 x = [1, 2, 3, 4, 5, 6]
 y = [1 ,2, 3, 4, 5, 6]
+z = [6, 5, 4, 3, 2, 1]
+
+#Takes as input the size of the array (i.e. how many classes), an array of the diversity indices for each class and an array containing the variable we want to compare it to. The function outputs the Pearson Correlation Index. 
 
 def pearson(size, arrayOfDivIndices, arrayOfCompVar):
+    #Calculate relevant values for both the diversity indices and the comparison values.
     xAvg = np.mean(arrayOfDivIndices)
     yAvg = np.mean(arrayOfCompVar)
     xStd = np.std(arrayOfDivIndices)
     yStd = np.std(arrayOfCompVar)
     
+    #Find the numerator of the Pearson Index.
     numerator = 0
     for i in range(size):
         numerator += (arrayOfDivIndices[i] - xAvg)*(arrayOfCompVar[i] - yAvg)
     
+    #Find the denominator of the Pearson Index.
     denom = (size-1)*xStd*yStd
     
+    #Calculate the Pearson Index.
     p = numerator/denom
     
     return p
@@ -44,15 +52,20 @@ def pearson(size, arrayOfDivIndices, arrayOfCompVar):
 #p = pearson(6, x, y)
 #print(p)
 
-#TODO: make something to check for and deal with repeated values. 
+"""TODO: make something to check for and deal with repeated values.""" 
+
+#Takes as input the size of the array (i.e. how many classes), an array of the diversity indices for each class and an array containing the variable we want to compare it to. The function outputs the Kendall Correlation Index.
+
 def kendall(size, arrayOfDivIndices, arrayOfCompVar):
-    
+    #Create counters to order the arrays.
     counter1 = 0
     counter2 = 0
     
+    #Create arrays that will contain ordered values.
     orderedArrayInd = [0 for j in range(size)]
     orderedArrayComp = [0 for m in range(size)]
     
+    #Order the diversity indices.
     i=0
     for i in range(size):
         if len(arrayOfDivIndices) != 0:
@@ -60,8 +73,9 @@ def kendall(size, arrayOfDivIndices, arrayOfCompVar):
                 orderedArrayInd[i] = counter1
                 arrayOfDivIndices = np.delete(arrayOfDivIndices, i)
                 counter1 += 1
-                print len(arrayOfDivIndices)
-        
+                print (len(arrayOfDivIndices))
+    
+    #Order the comparison value.  
     k=0
     for k in range(size):
         if len(arrayOfDivIndices) != 0:
@@ -70,30 +84,69 @@ def kendall(size, arrayOfDivIndices, arrayOfCompVar):
                 orderedArrayComp = np.delete(orderedArrayComp, k)
                 counter2 += 1
             
-            
+    #Create counters for concordant and discordant ordering of variables.         
     concordant = 0
     discordant = 0
     
+    #Count how many orderes are concordant and discordant. 
     for p in range(size):
         if orderedArrayInd[p] == orderedArrayComp[p]:
             concordant += 1
-            #print concordant
+            #print (concordant)
         else:
             discordant +=1
-            
+        
+    #Calculate the Kendall Correlation Coefficient. 
     t = 2*(concordant - discordant)/float((size*(size-1)))
 
     
     return t
 
 
-t = kendall(6, x, y)
+#t = kendall(6, x, y)
+#print (t)
 
-print t
+#Takes as input the size of the array (i.e. how many classes), an array of the diversity indices for each class and an array containing the variable we want to compare it to. The function outputs the Spearman Correlation Index.
+    
+def spearman(size, arrayOfDivIndices, arrayOfCompVar):
+    #Create arrays for ordered values.
+    nArrayOfDivIndices = np.zeros(size)
+    nArrayOfCompVar = np.zeros(size)
+    
+    #Create counters to represent the order of the values.
+    counter1 = 0
+    counter2 = 0
+    
+    #Create the ordered values of the diversity indices array.
+    for i in range(size):
+        if max(arrayOfDivIndices) == arrayOfDivIndices[i]:
+            nArrayOfDivIndices[i] = counter1
+            arrayOfDivIndices[i] = -1000000
+            counter1 += 1
+    
+    #Create the ordered values of the comparison value.
+    for j in range(size):
+        if max(arrayOfCompVar) == arrayOfCompVar[j]:
+            nArrayOfCompVar[i] = counter2
+            arrayOfCompVar[j] = -1000000
+            counter2 += 1
+        
+    #Find the numerator of the Spearman Index.
+    pNum = 0
+    for k in range(size):
+        d = nArrayOfDivIndices[k] - nArrayOfCompVar[k]
+        pNum += 6*d**2
+    
+    #Denominator of the Spearman Index.
+    pDenom = size**3 - size
+    
+    #Calculate the fraction of the Spearman Index.
+    p = float(pNum)/float(pDenom)
+    
+    return (1-p)
 
-
-
-
+#spearman = spearman(6, x, z)
+#print (spearman)
 
 
 
